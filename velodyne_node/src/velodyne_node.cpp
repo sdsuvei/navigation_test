fe6277428c3af1b0f3d20f3d3aa1ff5dd860af30
Mikkel Kragh Hansen <mkha@eng.au.dk>
2015-08-17 15:39:27 +0200

    Working with velodyne lidar online.

--- velodyne_node/src/velodyne_node.cpp
+++ velodyne_node/src/velodyne_node.cpp
@@ -23,6 +23,7 @@ union four_bytes
   };
 
 int ring_no;
+std::string pointcloud_in,pointcloud_out;
 int n;
 ros::Publisher output_;
 
@@ -30,26 +31,11 @@ ros::Publisher output_;
 
 void rawCloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloud)
 {
-//	ROS_INFO("received point cloud");
-
 	int NPackets = laserCloud->data.size();
-
 	int n;
-
-	//double x,y,z,intensity;
 	int ring;
-	//int baseIndex;
 
-	//const sensor_msgs::PointCloud2ConstPtr& laserCloudRing;
 	sensor_msgs::PointCloud cloud; //create empty pointcloud
-
-	// allocate a point cloud with same time and frame ID as raw data
-//	    velodyne_rawdata::VPointCloud::Ptr
-//	      outMsg(new velodyne_rawdata::VPointCloud());
-//	    // outMsg's header is a pcl::PCLHeader, convert it before stamp assignment
-//	    outMsg->header.stamp = pcl_conversions::toPCL(scanMsg->header).stamp;
-//	    outMsg->header.frame_id = scanMsg->header.frame_id;
-//	    outMsg->height = 1;
 
 	sensor_msgs::ChannelFloat32 intensities;
 	sensor_msgs::ChannelFloat32 index;
@@ -61,9 +47,6 @@ void rawCloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloud)
 	for (int p=0;p<NPackets;p+=32) // jump in steps of 32
 	{
 		ring = laserCloud->data[p+20]+laserCloud->data[p+21]*256; // (read the ring number)
-		//ROS_INFO("ring number = %d",ring);
-
-
 
 		if (ring == ring_no)
 		{
@@ -72,30 +55,36 @@ void rawCloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloud)
 			//velodyne_rawdata::VPoint test;
 			geometry_msgs::Point32 point;
 			union four_bytes tmp;
+
+			// x coordinate
 			tmp.bytes[0] = laserCloud->data[p];
 			tmp.bytes[1] = laserCloud->data[p+1];
 			tmp.bytes[2] = laserCloud->data[p+2];
 			tmp.bytes[3] = laserCloud->data[p+3];
 			point.x = tmp.floatV;
 
+			// y coordinate
 			tmp.bytes[0] = laserCloud->data[p+4];
 			tmp.bytes[1] = laserCloud->data[p+5];
 			tmp.bytes[2] = laserCloud->data[p+6];
 			tmp.bytes[3] = laserCloud->data[p+7];
 			point.y = tmp.floatV;
 
+			// z coordinate
 			tmp.bytes[0] = laserCloud->data[p+8];
 			tmp.bytes[1] = laserCloud->data[p+9];
 			tmp.bytes[2] = laserCloud->data[p+10];
 			tmp.bytes[3] = laserCloud->data[p+11];
 			point.z = tmp.floatV;
 
+			// intensity
 			tmp.bytes[0] = laserCloud->data[p+16];
 			tmp.bytes[1] = laserCloud->data[p+17];
 			tmp.bytes[2] = laserCloud->data[p+18];
 			tmp.bytes[3] = laserCloud->data[p+19];
 			intensities.values.push_back(tmp.floatV);
 
+			// index (increments for each point)
 			index.values.push_back(n);
 
 			cloud.points.push_back(point);
@@ -125,50 +114,15 @@ main (int argc, char** argv)
 	ros::init(argc, argv, "velodyne_node");
 	ros::NodeHandle nh;
 
-
-	/*ros::Publisher pubProcessedPointCloud = nh.advertise<sensor_msgs::PointCloud2>
-	("/point_cloud_processed", 1);*/
-
-
-	nh.param<int>("ring", ring_no, 15);
-
+	nh.param<int>("ring", ring_no, 15); // the ring/laser to use [0...31]
+	nh.param<std::string>("pointcloud_in",pointcloud_in,"/velodyne_points");
+	nh.param<std::string>("pointcloud_out",pointcloud_out,"/velodyne_1d_cloud");
 
 	ros::Subscriber subRawPointCloud = nh.subscribe<sensor_msgs::PointCloud2>
-	("/velodyne_points", 2, rawCloudHandler);
+	(pointcloud_in, 2, rawCloudHandler);
 	
-	output_ = nh.advertise<sensor_msgs::PointCloud>("velodyne_1d_cloud", 10);
+	output_ = nh.advertise<sensor_msgs::PointCloud>(pointcloud_out, 10);
 
-	/*
-	ros::Publisher pubOccupancyGrid = nh.advertise<nav_msgs::OccupancyGrid>
-	("/velodyne_evidence_grid", 1);
-
-	pubProcessedPointCloudPointer = &pubProcessedPointCloud;
-	subRawPointCloudPointer = &subRawPointCloud;
-	pubOccupancyGridPointer = &pubOccupancyGrid;
-
-	// Set up PCL Viewer
-	viewer.reset (new pcl::visualization::PCLVisualizer (argc, argv, "PointCloud"));
-	viewer->registerKeyboardCallback (keyboardEventOccurred);
-	//
-	//	// Create view ports
-	//	// 1 view port
-	viewer->createViewPort(0.0,0.0,1.0,1.0,v2);
-
-	// 2 view ports
-	//viewer->createViewPort(0.0,0.0,0.5,1.0,v1);
-	//viewer->createViewPort(0.5,0.0,1.0,1.0,v2);
-
-	// 4 view ports
-	//	viewer->createViewPort (0.0, 0.5, 0.5, 1.0, v1);
-	//	viewer->createViewPort (0.5, 0.5, 1.0, 1.0, v2);
-	//	viewer->createViewPort (0.0, 0.0, 0.5, 0.5, v3);
-	//	viewer->createViewPort (0.5, 0.0, 1.0, 0.5, v4);
-
-
-	viewer->addCoordinateSystem(1.0);
-
-	n = 0;
-	*/
 	ros::spin();
 
 	return (0);
